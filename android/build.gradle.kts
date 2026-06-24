367ca828606802a55c32b2f23645fbee78f183f4
Userio12 <freeredeemcode0777@gmail.com>
2026-06-24 09:27:14 +0530

    fix: resolve AGP namespace and Kotlin JVM target issues in CI

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,6 +16,23 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.library")) {
+            val androidExt = project.extensions.getByName("android") as com.android.build.api.dsl.LibraryExtension
+            if (androidExt.namespace == null) {
+                androidExt.namespace = project.group.toString()
+            }
+            androidExt.compileOptions {
+                sourceCompatibility = org.gradle.api.JavaVersion.VERSION_17
+                targetCompatibility = org.gradle.api.JavaVersion.VERSION_17
+            }
+        }
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
     project.evaluationDependsOn(":app")
 }
 
