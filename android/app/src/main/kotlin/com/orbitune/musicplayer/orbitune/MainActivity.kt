9464ee0cbf0666e687c95f5e8058749b2050316c
Userio12 <freeredeemcode0777@gmail.com>
2026-06-24 11:05:17 +0530

    fix: add audio_service manifest entries and MainActivity inheritance to resolve startup hang

--- android/app/src/main/kotlin/com/orbitune/musicplayer/orbitune/MainActivity.kt
+++ android/app/src/main/kotlin/com/orbitune/musicplayer/orbitune/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.orbitune.musicplayer.orbitune
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : AudioServiceActivity()
